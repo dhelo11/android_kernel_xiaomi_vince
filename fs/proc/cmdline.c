29ee67e5094df6663a063acc70dd41fd0382f00f
syahiddrakness <syahidselaluspd708@gmail.com>
2024-06-04 23:47:25 +0700

    Fix Error "proc: cmdline: Patch SafetyNet flags"
    Fixed Build Error For Commit 7442459
    
    Signed-off-by: negrroo <mohammedaelnaggar1@gmail.com>

--- fs/proc/cmdline.c
+++ fs/proc/cmdline.c
@@ -2,6 +2,7 @@
 #include <linux/init.h>
 #include <linux/proc_fs.h>
 #include <linux/seq_file.h>
+#include <asm/setup.h>
 
 static int cmdline_proc_show(struct seq_file *m, void *v)
 {
@@ -12,6 +13,30 @@ extern int susfs_spoof_cmdline_or_bootconfig(struct seq_file *m);
 #endif
 
 static char new_command_line[COMMAND_LINE_SIZE];
+
+#ifdef CONFIG_INITRAMFS_IGNORE_SKIP_FLAG
+#include <asm/setup.h>
+#endif
+
+#ifdef CONFIG_INITRAMFS_IGNORE_SKIP_FLAG
+#define INITRAMFS_STR_FIND "skip_initramf"
+#define INITRAMFS_STR_REPLACE "want_initramf"
+#define INITRAMFS_STR_LEN (sizeof(INITRAMFS_STR_FIND) - 1)
+
+static char proc_command_line[COMMAND_LINE_SIZE];
+
+static void proc_command_line_init(void) {
+	char *offset_addr;
+
+	strcpy(proc_command_line, saved_command_line);
+
+	offset_addr = strstr(proc_command_line, INITRAMFS_STR_FIND);
+	if (!offset_addr)
+		return;
+
+	memcpy(offset_addr, INITRAMFS_STR_REPLACE, INITRAMFS_STR_LEN);
+}
+#endif
 
 static int cmdline_proc_show(struct seq_file *m, void *v)
 {
