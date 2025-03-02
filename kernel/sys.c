923e8addc64d33b63b848edb0898b0588e67719b
Rapli Kunz <raffikun1203@gmail.com>
2025-03-02 14:08:22 +0000

    Add sUsFS 1.5.5

--- kernel/sys.c
+++ kernel/sys.c
@@ -1143,12 +1143,34 @@ static int override_release(char __user *release, size_t len)
 	return ret;
 }
 
+static void override_custom_release(char __user *release, size_t len)
+{
+    char *buf;
+
+    buf = kstrdup_quotable_cmdline(current, GFP_KERNEL);
+    if (buf == NULL)
+        return;
+
+    if (strncmp(buf, "com.google.android.gms", 22) == 0) {
+            copy_to_user(release, MOD_KERNEL_VERSION, strlen(MOD_KERNEL_VERSION));
+    }
+
+    kfree(buf);
+}
+
+#ifdef CONFIG_KSU_SUSFS_SPOOF_UNAME
+extern void susfs_spoof_uname(struct new_utsname* tmp);
+#endif
+
 SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 {
 	struct new_utsname tmp;
 
 	down_read(&uts_sem);
 	memcpy(&tmp, utsname(), sizeof(tmp));
+#ifdef CONFIG_KSU_SUSFS_SPOOF_UNAME
+	susfs_spoof_uname(&tmp);
+#endif
 	up_read(&uts_sem);
 	if (copy_to_user(name, &tmp, sizeof(tmp)))
 		return -EFAULT;
