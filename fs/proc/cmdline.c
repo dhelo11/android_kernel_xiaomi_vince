3fa8821515293e9879fc25687d2c2f3274d09bc2
Rapli Kunz <raffikun1203@gmail.com>
2025-03-02 14:09:08 +0000

    cmdline: Add Support For sUsFS

--- fs/proc/cmdline.c
+++ fs/proc/cmdline.c
@@ -3,9 +3,31 @@
 #include <linux/proc_fs.h>
 #include <linux/seq_file.h>
 
+
 static int cmdline_proc_show(struct seq_file *m, void *v)
 {
 	seq_printf(m, "%s\n", saved_command_line);
+
+#ifdef CONFIG_KSU_SUSFS_SPOOF_CMDLINE_OR_BOOTCONFIG
+extern int susfs_spoof_cmdline_or_bootconfig(struct seq_file *m);
+#endif
+
+static char new_command_line[COMMAND_LINE_SIZE];
+
+static int cmdline_proc_show(struct seq_file *m, void *v)
+{
+#ifdef CONFIG_KSU_SUSFS_SPOOF_CMDLINE_OR_BOOTCONFIG
+	if (!susfs_spoof_cmdline_or_bootconfig(m)) {
+		seq_putc(m, '\n');
+		return 0;
+	}
+#endif
+#ifdef CONFIG_INITRAMFS_IGNORE_SKIP_FLAG
+	seq_printf(m, "%s\n", proc_command_line);
+#else
+	seq_printf(m, "%s\n", new_command_line);
+#endif
+
 	return 0;
 }
 
